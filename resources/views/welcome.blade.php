96f33f8d25b3e8c6ea306556ca5469c522744a08
chiba008007 <chiba00807@gmail.com>
2024-10-13 20:00:40 +0900

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,10 +17,6 @@
     </head>
     <body class="antialiased">
 
-        @php
-        echo "aaaaaaa";
-        var_dump(config('const.consts.LISENCE'));
-        @endphp
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">

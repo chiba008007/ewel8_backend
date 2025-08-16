0179e9d708800674b35beaef6f43a8fa6a72ee4e
chiba008007 <chiba00807@gmail.com>
2025-08-17 00:18:15 +0900

    修正

--- app/Http/Controllers/PDF/IndexController.php
+++ app/Http/Controllers/PDF/IndexController.php
@@ -14,14 +14,15 @@ class IndexController extends Controller
 
     public function index(Request $request, $id, $code, $birth, $encode)
     {
-
+        echo "test";
+        exit();
         Log::info('PDF@index called', compact('id', 'code', 'birth', 'encode'));
 
         try {
-            //$this->checkedCode($encode, $code);
+            $this->checkedCode($encode, $code);
 
             // PFS用のチャートグラフを生成するよう
-            //require_once(public_path()."/PDF/pfsCreateGraph.php");
+            require_once(public_path()."/PDF/pfsCreateGraph.php");
             $obj = new pdfs();
             $pdf = $obj->addPageToPdf($id, $code, $birth);
 

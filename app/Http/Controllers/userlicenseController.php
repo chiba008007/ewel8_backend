90d5269adfdab0b2ebb07eecf12079688053b52f
chiba008007 <chiba00807@gmail.com>
2025-11-09 18:33:33 +0900

    ライセンス

--- app/Http/Controllers/userlicenseController.php
+++ app/Http/Controllers/userlicenseController.php
@@ -11,32 +11,41 @@ class userlicenseController extends Controller
     //
     public function list()
     {
-        $results = userlisence::with(['triggerHistories','examLogs'])
-        ->select('code', DB::raw('SUM(num) as total_num'))
-        ->groupBy('code')
+        $results = UserLisence::with(['triggerHistories', 'examLogs'])
         ->get()
-        ->map(function ($item) {
-            $add = $item->triggerHistories
+        ->groupBy('code')
+        ->map(function ($group) {
+            $first = $group->first();
+
+            $total_num = $group->sum('num');
+
+            $add = $first->triggerHistories
                 ->where('status', 'add')
                 ->sum('num');
 
-            $delete = $item->triggerHistories
+            $delete = $first->triggerHistories
                 ->where('status', 'delete')
                 ->sum('num');
 
-            $item->exam_count = $add - $delete;
-            $item->available_license = $item->total_num - $item->exam_count;
+            $exam_count = $add - $delete;
+            $available_license = $total_num - $exam_count;
 
-            // exam_logs の処理件数（status = 1 または 2）
-            // Laravel の Eloquent は、リレーションを明示的に with() でロードしていなくても、
-            // 初めてそのプロパティ（$item->examLogs）にアクセスした瞬間に
-            // 自動でSQLを発行して取得してくれます。
-            $item->syori_count = $item->examLogs
+            $syori_count = $first->examLogs
                 ->whereIn('status', [1, 2])
                 ->count();
-            $item->zan = $item->exam_count - $item->syori_count;
-            return $item;
-        });
+
+            $zan = $exam_count - $syori_count;
+
+            return (object)[
+                'code' => $first->code,
+                'total_num' => $total_num,
+                'exam_count' => $exam_count,
+                'available_license' => $available_license,
+                'syori_count' => $syori_count,
+                'zan' => $zan,
+            ];
+        })
+        ->values();
 
         return response()->json([
             'result' => true,
